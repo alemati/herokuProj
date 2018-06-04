a07eae5b9907b4b21f2e223fc55e186e4d17edec
Aleksander Matikainen <AM@Aleksander-MacBook-Pro.local>
2018-06-04 17:36:45 +0300

    index

--- src/main/java/hp/herokuproj/Main.java
+++ src/main/java/hp/herokuproj/Main.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hp.herokuproj;
 
-/**
- *
- * @author AM
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        if (System.getenv("PORT") != null) {
+            Spark.port(Integer.valueOf(System.getenv("PORT")));
+        }
+        
+        Spark.get("*", (req, res) -> {
+            
+            HashMap map = new HashMap<>();
+            map.put("teksti", "spark Get toimii!!");
+
+            return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
+        
+    }
+
 }
