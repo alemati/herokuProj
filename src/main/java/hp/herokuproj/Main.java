9deb5ef0726ef45be24b740b643c2695b564e246
Aleksander Matikainen <AM@Aleksander-MacBook-Pro.local>
2018-06-04 21:19:06 +0300

    postit

--- src/main/java/hp/herokuproj/Main.java
+++ src/main/java/hp/herokuproj/Main.java
@@ -21,22 +21,22 @@ public class Main {
         
         RaakaAineDao raakaAineDao = new RaakaAineDao();
         
-        Spark.get("*", (req, res) -> {
+        Spark.get("/*", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("raakaAineLista", raakaAineDao.findAll());
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
         
-        Spark.post("*", (req, res) -> {
+        Spark.post("/*", (req, res) -> {
             if (raakaAineDao.findAll() == null) {
                 raakaAineDao.save(new RaakaAine(1, req.queryParams("nimi")));
             } else {
                 raakaAineDao.save(new RaakaAine(raakaAineDao.viimeinenId() + 1, req.queryParams("nimi")));
             }
-            res.redirect("*");
+            res.redirect("/*");
             return "";
         });
-        Spark.post("*/poisto/:id", (req, res) -> {
+        Spark.post("/*/poisto/:id", (req, res) -> {
             int id = Integer.parseInt(req.params(":id"));
             raakaAineDao.delete(id);
             res.redirect("/*");
