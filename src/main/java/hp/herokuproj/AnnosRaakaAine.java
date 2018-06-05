d833897b3782a59dfc4861bb3b2255132fd04f21
Aleksander Matikainen <AM@Aleksander-MacBook-Pro.local>
2018-06-05 16:50:52 +0300

    index3

--- src/main/java/hp/herokuproj/AnnosRaakaAine.java
+++ src/main/java/hp/herokuproj/AnnosRaakaAine.java
@@ -4,13 +4,11 @@ package hp.herokuproj;
 public class AnnosRaakaAine {
     private int annosId;
     private int raakaAineId;
-    private int jarjestys;
     private String maara;
     private String ohje;
-    public AnnosRaakaAine(int annosId, int raakaAineId, int jarjestys, String maara, String ohje) {
+    public AnnosRaakaAine(int annosId, int raakaAineId, String maara, String ohje) {
         this.annosId = annosId;
         this.raakaAineId = raakaAineId;
-        this.jarjestys = jarjestys;
         this.maara = maara;
         this.ohje = ohje;
     }
@@ -19,9 +17,6 @@ public class AnnosRaakaAine {
     }
     public int getRaakaAineId() {
         return this.raakaAineId;
-    }
-    public int getJarjestys() {
-        return this.jarjestys;
     }
     public String getMaara() {
         return this.maara;
